d2deeb6e522357faf20ebde139a2a5d2f148f428
Antoine.Guillaume <antoineguillaume6356@outlook.fr>
2022-03-17 09:44:48 +0100

    mon commit prog 1 du 100 jours

---
+++ Prog1/Prog1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Prog1
+//
+//  Created by Antoine Guillaume on 16/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Prog1/Prog1/Prog1App.swift
@@ -0,0 +1,17 @@
+//
+//  Prog1App.swift
+//  Prog1
+//
+//  Created by Antoine Guillaume on 16/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct Prog1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
