5e02bfdf26f087e387a0ac9c251b763c3d765998
Antoine.Guillaume <antoineguillaume6356@outlook.fr>
2022-03-17 09:59:52 +0100

    j'ai rajouter un ptit print et un ptit doss.

---
+++ Prog1/Prog1/Programme1.swift
@@ -0,0 +1,16 @@
+//
+//  Programme1.swift
+//  Prog1
+//
+//  Created by Antoine Guillaume on 17/03/2022.
+//
+
+import Foundation
+
+class Programme1 {
+    var age = 0
+}
+
+func age() {
+    print("ouais ouais.")
+}
